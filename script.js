c2349eb1458ffb75ff2cbbbadcb7a8b92e63aac2
Aleksandr <sasha.bulanov@inbox.lv>
2020-04-11 19:55:39 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var number = 5;
+var string = "Hello world!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+// let answer = +prompt("Are you 18 year-old?", "Yes");
+
+// console.log(typeof(answer));
+
+// console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(!isChecked || isClose);
